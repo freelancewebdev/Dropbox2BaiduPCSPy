5ef11294039d416927dc797b5dffe193e3c07af0
Joe Molloy <molloyjoe@gmail.com>
2014-05-26 00:09:54 +0100

    First commit

---
+++ Dropbox2BaiduPCS.py
@@ -0,0 +1,386 @@
+# -*- coding: utf-8 -*-
+import ConfigParser
+import dropbox
+from baidupcs import PCS
+import urllib2
+import urllib
+import json
+import sys
+import os
+import time
+import codecs
+
+print 'Dropbox 2 Baidu PCS Py'  
+print 'Copyright (C) 2014  Joe Molloy (info[at]hyper-typer.com)'
+print 'This program comes with ABSOLUTELY NO WARRANTY.'
+print 'This is free software, and you are welcome to redistribute it'
+
+db_app_key = ''
+db_app_secret = ''
+db_access_token = ''
+b_app_key = ''
+b_app_secret = ''
+b_access_token = ''
+b_folder = ''
+ignore_folders = []
+secure_mode = False
+
+
+localpath = os.path.dirname(os.path.abspath(__file__))
+filecount = 0
+filesize = 0
+logfilepath = localpath + '/log.txt'
+errcount = 0
+dbclient = dropbox.client
+dbpath = '/'
+curdbpath = '/'
+curbpath = ''
+bpath = ''
+
+tempdir = localpath + '/temp'
+
+def sizeof_fmt(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
+def local_setup():
+	global db_app_key, db_app_secret, db_ignore_folders, db_access_token, b_app_key, b_app_secret, b_folder, b_access_token, ignore_folders, secure_mode, curbpath, bpath
+	print 'Preparing local system...'
+	if not os.path.exists(tempdir):
+		print 'Creating temporary folder...'
+		try:
+			os.makedirs(tempdir)
+			print 'Local system ready!'
+		except IOError:
+			print 'Could not create temporary folder. Bye!'
+			sys.exit()
+	print 'Preparing log file...'
+	if not os.path.exists(logfilepath):
+		print 'Creating log file...'
+		try:
+			f = open(logfilepath,'w')
+			f.write('Starting At: ' + time.strftime("%c"))
+			f.close()
+		except IOError:
+			print 'Could not create log file. Bye!'
+			sys.exit()
+	else:
+		print 'Initialising log file...'
+		f = open(logfilepath,'w')
+		f.write('Starting At: ' + time.strftime("%c"))
+		f.close()
+	print 'System setup complete.  Ready to start!'
+	print 'Checking configuration data...'
+	cfg = ConfigParser.ConfigParser()
+	try:
+		cfg.read('Dropbox2BaiduPCS_sample.cfg')
+	except:
+		print 'There was a problem reading the config file'
+		print 'Please ensure you have renamed'
+		print '\'Dropbox2BaiduPCS_sample.cfg\' to \'pymygreater.cfg\''
+		print 'and added your own values as appropriate'
+		sys.exit()
+	try:
+		db_app_key = cfg.getstring('dropbox','db_app_key')
+	except:
+		print 'No Dropbox application key set in config file'
+		sys.exit()
+	if not db_app_key:
+		print 'No Dropbox application key set in config file'
+		sys.exit();
+	else:
+		print 'db_app_key is ' + db_app_key
+	try:
+		db_app_secret = cfg.getstring('dropbox','db_app_secret')
+	except:
+		print 'No Dropbox application secret set in config file'
+		sys.exit()		
+	if not db_app_secret:
+		print 'No Dropbox applicatiolication secret set in config file'
+		sys.exit()
+	try:
+		b_app_key = cfg.getstring('baidu','b_app_key')
+	except:
+		print 'No Baidu application key set in config file'
+		sys.exit()
+	if not b_app_key:
+		print 'No Baidu application key set in config file'
+		sys.exit()
+	try:
+		b_app_secret = cfg.getstring('baidu','b_app_secret')
+	except:
+		print 'No Baidu application secret set in config file'
+		sys.exit()
+	if not b_app_secret:
+		print 'No Baidu application secret set in config file'
+		sys.exit()
+	try:
+		b_folder = cfg.getstring('baidu','b_folder')
+	except:
+		print 'No Baidu applicatio folder set in config file'
+		sys.exit()
+	if not b_folder:
+		print 'No Baidu application folder set in config file'
+		sys.exit()
+	db_ignore_folders = cfg.getstring('dropbox','db_ignore_folders')
+	if db_ignore_folders:
+		ignore_folders = db_ignore_folders.split(',')
+	else:
+		ignore_folders = []
+	try:
+		secure_mode = cfg.getboolean('app','secure_mode')
+	except:
+		pass
+	try:
+		db_access_token = cfg.getstring('dropbox','db_access_token')
+	except:
+		pass
+	try:
+		b_access_token = cfg.getstring('baidu','b_access_token')
+	except:
+		pass
+	curbpath = '/apps/' + b_folder + '/dropbox'
+	bpath = '/apps/' + b_folder + '/dropbox'
+
+def db_auth(db_app_key,db_app_secret):
+	global db_access_token
+	print 'Authenticating with Dropbox'
+	print 'Checking for previously obtained Dropbox access token...'
+	if db_access_token: 
+		print 'Previously obtained Dropbox access token found!'
+	else:
+		flow = dropbox.client.DropboxOAuth2FlowNoRedirect(db_app_key, db_app_secret)
+		authorize_url = flow.start()
+		print '1. Go to: ' + authorize_url
+		print '2. Click "Allow" (you might have to log in first)'
+		print '3. Copy the authorization code.'
+
+		code = raw_input("Enter the authorization code here: ").strip()
+		db_access_token, user_id = flow.finish(code)
+		cfg = ConfigParser.ConfigParser()
+		cfg.set('dropbox','db_access_token',db_access_token)
+		cfgfile = open('Dropbox2BaiduPCS_sample.cfg','w')
+		cfg.write(cfgfile)
+		cfgfile.close()
+	dbclient = dropbox.client.DropboxClient(db_access_token)
+	dbclientinfo = dbclient.account_info()
+	print 'Dropbox Linked Account: ', dbclientinfo['display_name']
+	print 'Dropbox Quota: ' + sizeof_fmt(dbclientinfo['quota_info']['quota'])
+	print 'Dropbox Used: ' + sizeof_fmt(dbclientinfo['quota_info']['normal'])
+	print 'Dropbox Ready!'
+	return dbclient
+
+def b_auth(b_app_key,b_app_secret):
+	global b_access_token
+	print 'Authenticating with Baidu'
+	if not b_access_token
+		code_url = 'https://openapi.baidu.com/oauth/2.0/device/code?'
+		code_url += 'client_id=' + b_app_key
+		code_url += '&response_type=device_code'
+		code_url += '&scope=basic netdisk'
+		response = urllib2.urlopen(code_url)
+		data = json.load(response)
+		reply = raw_input("Enter 'y' here when you have submitted the user code '" + data['user_code'] + "' at " + data['verification_url']).strip()
+		if reply == 'y':
+			code_url = 'https://openapi.baidu.com/oauth/2.0/token?'
+			code_url += 'grant_type=device_token' 
+			code_url += '&code=' + data['device_code']
+			code_url += '&client_id=' + b_app_key
+			code_url += '&client_secret=' + b_app_secret
+			response1 = urllib2.urlopen(code_url)
+			data1 = json.load(response1)
+			if not ('error' in data1):
+				b_access_token = data1['access_token']
+				cfg = ConfigParser.ConfigParser()
+				cfg.set('Baidu',b_access_token)
+				cfgfile = open('Dropbox2BaiduPCS.cfg','w')
+				config.write(cfgfile)
+				cfgfile.close()
+			else:
+				reply = raw_input("Authentication failed.  Do you wish to try again? (y/n)").strip()
+				if reply == 'y':
+					b_auth(b_app_key,b_app_secret)
+				else:
+					print 'OK, bye!'
+					sys.exit()
+
+		else:
+			reply = raw_input("Do you wish to try to authorise with Baidu again? (y/n)").strip()
+			if reply == 'y':
+				b_auth(b_app_key,b_app_secret)
+			else:
+				print 'OK, bye!'
+				sys.exit()
+	if(b_access_token != ''):
+		pcs = PCS(b_access_token)
+		qresponse = pcs.info()
+		qdata = qresponse.json()
+		print 'Baidu Quota: ' + sizeof_fmt(float(qdata["quota"]))
+		print 'Baidu Used: ' + sizeof_fmt(float(qdata['used']))
+		qdata = pcs.meta('/apps/mygreater/dropbox').json();
+		if ('error_code' in qdata):
+			print 'Creating Dropbox folder on Baidu...'
+			qdata = pcs.mkdir(bpath)
+			print 'Baidu Ready!'
+		else:
+			print 'Baidu Ready!'
+		return pcs
+
+
+
+
+def list_dirs(path):
+	print 'Folders found in ' + path
+	subdirs = [d['path'] for d in folder_metadata['contents'] if d['is_dir'] == True]
+	for sd in subdirs:
+		print sd
+
+def list_files(path):
+	global filecount, bpath, dbpath, curbpath, curdbpath
+	for f in ignore_folders:
+		if f in path:
+			print 'Ignoring \'' + f + '\''
+			return
+	try:
+		folder_metadata = dbclient.metadata(curdbpath)
+	except:
+		time.sleep(30)
+		list_files(path)
+	print 'Checking for files in \'' + path + '\' on Dropbox'
+	subfiles = [d['path'] for d in folder_metadata['contents'] if d['is_dir'] == False]
+	if(len(subfiles) > 0):
+		for f in subfiles:
+			filename = dbdownload(f)
+			bupload(filename)
+			filecount += 1
+	else:
+		print 'No files found.'
+	print 'Checking for directories in \'' + path + '\' on Dropbox'
+	subdirs = [d['path'] for d in folder_metadata['contents'] if d['is_dir'] == True]
+	if(len(subdirs) > 0):
+		for d in subdirs:
+			print 'Looking at the \'' + d + '\' folder'
+			curbpath = bpath + d
+			curdbpath = d
+			try:
+				data = pcs.meta(curbpath).json();
+			except:
+				print 'Trying again in 30 seconds...'
+				time.sleep(30)
+				list_files(path)
+			if ('error_code' in data):
+				print 'Creating \'' + curbpath + '\' folder on Baidu...'
+				try:
+					data = pcs.mkdir(curbpath.encode('utf-8'))
+					print '\'' + d + '\' folder created on Baidu...'
+				except:
+					print 'There was a problem creating the \'' + curbpath + '\' directory on Baidu'
+					print 'Trying again in 30 seconds'
+					time.sleep(30)
+					list_files(path)
+			try:
+				list_files(curdbpath)
+			except:
+				print 'Trying again in 30 seconds...'
+				time.sleep(30)
+				list_files(path)
+	else:
+		print 'No directories found in \'' + path + '\' on Dropbox'
+	if(curbpath != bpath):
+		curdbpath = curdbpath.rsplit('/', 1)[0] 
+		if(curdbpath == ''):
+			curdbpath = '/'
+		print 'Moving back up to ' + curdbpath + ' on Dropbox'
+		curbpath = curbpath.rsplit('/', 1)[0]
+		print 'Moving back up to ' + curbpath + ' on Baidu'
+
+
+
+def dbdownload(filepath):
+	global filesize
+	filename = os.path.basename(filepath)
+	try:
+		f, metadata = dbclient.get_file_and_metadata(filepath)
+		print 'Downloading ' + filename
+	except:
+		print filename + ' not found'
+		return False
+	try:
+		out = open(tempdir + '/' + filename, 'wb')
+		out.write(f.read())
+		out.close()
+		print filename + ' downloaded'
+		filesize = filesize + os.path.getsize(tempdir + '/' + filename)
+		return filename
+	except IOError:
+		print 'Problem saving ' + filename
+		return False
+
+def bupload(filename):
+	global errcount
+	if filename != False:
+		print 'Preparing ' + filename + ' for upload...'
+		try:
+			fc = open(tempdir + '/' + filename, 'rb')
+		except IOError:
+			print 'Problem opening ' + filename
+			return False
+		try:
+			response = pcs.upload((curbpath + '/' + filename).encode('utf-8'),fc, 'overwrite')
+		except:
+			if errcount < 10:
+				print 'Ooops, issues encountered while uploading \'' + filename + '\' - retrying (try ' + str(errcount + 1) + ' of 10)...'
+				errcount += 1
+				time.sleep(30)
+				response = pcs.upload((curbpath + '/' + filename).encode('utf-8'),fc, 'overwrite')
+			else:
+				print 'Ooops, there was a problem uploading ' + filename + ' to Baidu'
+				f = codecs.open(logfilepath,'a', encoding='utf-8')
+				f.write('Baidu Upload Failure: ' + curbpath + '/' + filename)
+				f.close()
+				return False
+		if('error_code' in response.json()):
+			print 'Ooops, there was a problem uploading ' + filename + ' to Baidu'
+			f = codecs.open(logfilepath,'a', encoding='utf-8')
+			f.write('Baidu Upload Failure: ' + curbpath + '/' + filename)
+			f.close()
+			return False
+		else:
+			print filename + ' uploaded to Baidu'
+		fc.close()
+		os.remove(tempdir + '/' + filename)
+
+
+def do_cleanup():
+	print 'Almost done, cleaning up...'
+	for the_file in os.listdir(tempdir):
+		file_path = os.path.join(tempdir, the_file)
+    	try:
+        	if os.path.isfile(file_path):
+        		os.unlink(file_path)
+    	except Exception, e:
+        		print e
+	print 'Temporary files deleted'
+	if secure_mode:
+		print 'Deleting access token files...'
+		os.unlink(localpath + '/token')
+		os.unlink(localpath + '/btoken')
+		print 'Access token files deleted'
+
+local_setup()
+dbclient = db_auth(db_app_key,db_app_secret)
+pcs = b_auth(b_app_key,b_app_secret)
+list_files(dbpath)
+do_cleanup()
+print 'Closing log file...'
+f = open(logfilepath,'a')
+f.write('Finishing At: ' + time.strftime("%c"))
+f.close()
+print 'Logging done!'
+if(filecount != 1):
+	print str(filecount) + ' files uploaded (' + sizeof_fmt(filesize) + ')'
+else:
+	print str(filecount) + ' file uploaded (' + sizeof_fmt(filesize) + ')'
---
+++ readme.md
@@ -0,0 +1,96 @@
+#Dropbox2BaiduPCSPy
+Python based Dropbox to Baidu PCS Data Migration Script
+
+##Introduction
+Chinese based internet services provider giant [Baidu](http://www.baidu.com/) offers a cloud based file storage service very similar to that offered by [Dropbox](http://www.dropbox.com/) called [Baidu PCS](http://pan.baidu.com/ "Baidu Personal Cloud Service").  However, in contrast with the service from Dropbox, which maxes out at 16GB for their free tier, most of which one has to earn via referrals, Baidu's offering comes with a whopping 2TB of free storage space without requiring jumping through any promotional hoops to access all that storage.  Even though the service is exclusively offered in the Chinese language at this point, the service is very usable using their browser based interface with Chrome and it's translation facility.  Baidu also provides clients for all the major desktop and mobile platforms which even non-chinese speakers can figure out with a little trial and error.  In 2013, Baidu tentatively began to engage with Western based developers by providing an [English language entrance to their developer portal](http://developer.baidu.com/en/) which seems to suggest that English langauge support for their services may be on the way. Even if you don't find the service suitable for every day use, it is still a pretty attractive option for archiving older less frequently needed data given the massive free disk space allowance.  Those of you concerned with the data security implications of storing your information on Chinese controlled servers might want to consider how likely it is that Baidu will cooperate with the NSA.
+
+This simple Python 2.6 script makes use of the API's provided by Dropbox and Baidu to allow you to automate the migration of data from Dropbox to Baidu.  While I am not primarily a Python developer and while I cannot and do not claim that it has been exhaustively tested, this script has been used successfully on Mac and Linux to transfer around 27GB of my own personal data, including all kinds and sizes of files from spreadsheets to MP4 videos, some with filenames which had accented characters (so it should be fully UTF-8 compliant).  You should note that currently it may need some adjustments to run on Windows - yet to be tested.  
+
+It comes out of the box with some simple logging, noting the start and end times of the script's execution and listing files it had issues migrating.  I have also added some simple retry logic where required to handle brief network outages.  It takes care of the process of obtaining user authentication tokens itself, guiding the user as required. It only requires that you have a minimum few elements in place in advance to successfully use it.
+
+1. A Dropbox account with data to migrate (doh)
+2. A Dropbox application with credentials to use their API
+3. A Baidu PCS account where your data will be stored.
+4. A Baidu application with credentials and permissions to use their API
+5. Installed script dependencies
+6. A configured version of the Python script.
+
+I will assume you already have a Dropbox account set up and configured, otherwise there wouldn't be much point in using this script.  See below for details on getting the other elements in place.
+
+##Preperation
+###Setting Up a Dropbox Application
+To set up your Dropbox application, go to the [Dropbox developers portal](https://www.dropbox.com/developers) and log in with your Dropbox credentials if required.  From there, click the 'App Console' link on the left and choose to create a new Dropbox API app. Choose the files and datastores option under the data type your application needs to access, choose NOT to limit your app to its own folder and choose the option to allow your app to access all file types. Finally, choose a name for your app, preferably something meaningful, and you are done with your Dropbox application setup.  Just make a note of your App Key and App Secret as you will need them to configure the script in a bit.
+
+###Setting Up a Baidu PCS Account
+Unless you read Chinese, I would recommend you use the [Google Chrome Web Browser](http://www.google.com/chrome) to complete this series of steps to set up your Baidu PCS account as, as previously mentioned, it comes with built in web page translation.
+Go to the [Baidu PCS Homepage](https://pan.baidu.com) and register using the button provided.  You can give either your email address OR a Chinese mobile number so most of you will probably give an email address, password and complete the captcha as required.  After submitting the registration form you will then be informed that you must activate your account fully by following the link sent to you via email.  Do so and your Baidu account is ready for storing your data.
+
+###Setting Up Your Baidu Developer Account
+Now that you have a Baidu account you may become a Baidu developer.  While logged in to your Baidu PCS account, simply head on over to the [Baidu Developer Portal](https://developer.baidu.com/) and choose the 'Registered Developer' link from the dropdown menu which appears when you hover over your username in the top navigation menu which will take you to the [Baidu Developer registration form](http://developer.baidu.com/user/reg).
+The form is pretty self explanatory.  To complete it successfully you must supply your mobile phone number so that you can obtain the verification code required to complete the registration process - you should note that only one developer account can be created per phone number.  Once the registration process is complete you can proceed to set up your application.
+
+###Setting up Your Baidu Application
+This is probably the most challenging part of the process, simply because of the language difficulties and the effect that the Google Translator has on the layout of the pages in the Baidu Developer Application Management Console.  I will try to be as clear as possible.
+If you haven't already, log into the [Baidu developer portal](https://developer.baidu.com/) using the credentials of the account you created above and then click on the 'Management Console' link in the top navigation menu which will lead you [here](http://developer.baidu.com/console#app/project).  Then click the 'Create Project' button which will take you to [this screen](http://developer.baidu.com/console#app/create) where you can proceed to configure your application.  The only thing you need to do on this screen is name your app something meaningful.
+
+When you submit the name for your app, the resulting screen will give you your API Key and Secret Key and you should note those for later use in the script.
+
+Also on this page you will notice a list of 'Developer Services' either above or to the left of your app details (depending on how Google Chrome renders the screen layout).  In order for your newly created app to function, you must explicitly obtain authorisation to use the PCS API.  It seems this authorisation is currently manually processed by Baidu.
+
+Click on the 'Other API' link from the list of 'Developer Services'. First check the checkbox beside the PCS item in the resulting API list and then click on the 'Open' button at the top of the list.  When you do this, a popup window will appear with a short form which must be completed basically requiring you to enter the following details about your application in the 3 fields provided:
+
+1. The folder name for your app
+Unlike Dropbox, currently each app that uses the Baidu API is sandboxed to its own folder in the user's storage and cannot modify files outside this folder.  Usually you would name the folder after your app.  I recommend you stick to ASCII letters and digits only for the name.
+2. A brief description of your app.  At this point I have submitted descriptions in both English and Chinese (via Google translate) and both apps were authorised without a hitch.  I basically said I was using the API for personal use and educational purposes.
+3. You must provide 3 pieces of information in the last field:
+...1. The estimated number of users of your app - in this case it will be only you so one would seem an appropriate answer.
+...2. An estimate of the storage required - I basically doubled the space I had occupied on Dropbox as an estimate but implicitly said that this was only an estimate.
+...3. An estimate of the traffic/bandwidth your user(s) will generate - I put this at 30GB monthly without issue.
+
+Please note that sometimes the ability to 'Open' the PCS API authorisation is disabled temporarily but in my experience these outages do not last more than a couple of days, so if you find this facility disabled, check back regularly for changes.
+
+Finally, click the 'Security Settings' button also in the 'Other API' screen at the tope of the API list.  Choose to DISABLE implicit grant authorisation in the resulting form and then click the 'Determine' button to submit your change - the implicit grant authorisation method is used by websites to obtain authorisation tokens to access their users' accounts but in this case we are using a device authorisation so the implicit grant process is not used and those details are not required.
+
+###Installing Script Dependencies
+You will need the [Baidu Python PCS](https://pypi.python.org/pypi/baidupcs/0.3.1) and the [Dropbox](https://www.dropbox.com/developers/core/sdks/python) modules.  Instructions for installing these modules on different platforms are discussed at each of the links provided and are beyond the scope of this project to provide.
+
+###Configuring the Script
+You should edit the config file Dropbox2BaiduPCS_sample.cfg adding your own values obtained above and the save the file as Dropbox2BaiduPCS.cfg in the same directory as you are runnig Dropbox2BaiduPCS.py.  The following values are required:
+
+1. db\_app\_key: 
+Your Dropbox app key obtained above (string)
+2. db\_app\_secret
+Your Dropbox app secret obtained above (string)
+3. b\_app\_key: 
+Your Baidu app key obtained above (string)
+4. b\_app\_secret: 
+Your Baidu app secret obtained above (string)
+5. b\_folder: 
+Your Baidu app folder name as configured on Baidu above.
+6. db\_ignore\_folders: 
+A comma separated list of folder names which you wish to ignore in your migration - paths 
+including any of these names will be skipped.  If you do not wish to skip any folders you must provide an empty string, i.e. ignorefolders = ''. (string)
+7. secure\_mode: 
+By default this setting is false which means that once the script has obtained the access tokens to use the Dropbox and Baidu API's, it will store them in config file as plaintext.  On subsequent runs of this script, the access tokens will be read from these files and you will not have to go through the authentication process again.  However, storing these tokens is plain text is not very secure so you have the option of not persisting this information to disk by setting the 'secure_mode' configuration variable to True. (boolean)
+
+###Running the Script
+Just call python Dropbox2BaiduPCS.py
+
+###Licensing
+Copyright (C) 2014  Joe Molloy
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+
+
+
+
+
+
+
+
